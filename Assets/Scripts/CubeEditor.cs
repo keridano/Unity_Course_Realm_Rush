c179ac0261ec3825d8b820cc33c16b4367166de8
Enrico <keridano@gmail.com>
2020-04-28 00:05:28 +0200

    Cube now has got text mesh auto updating with snaps

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -1,17 +1,24 @@
 ﻿using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Range(1f,20f)] float gridSize = 10f;
 
+    TextMesh textMesh;
+
     void Update()
     {
+        textMesh = GetComponentInChildren<TextMesh>();
+
         transform.position = new Vector3
         {
             x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
             y = 0f,
             z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
         };
+
+        textMesh.text = $"{transform.position.x / gridSize},{transform.position.z / gridSize}";
     }
 }
