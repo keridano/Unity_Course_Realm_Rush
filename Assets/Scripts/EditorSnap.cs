dffba3dd62c9c1da33a73be4b2532add0bb5743f
Enrico <keridano@gmail.com>
2020-04-27 23:24:41 +0200

    Cube automatic snaps to grid

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] [Range(1f,20f)] float gridSize = 10f;
+
+    void Update()
+    {
+        transform.position = new Vector3
+        {
+            x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
+            y = 0f,
+            z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
+        };
+    }
+}
