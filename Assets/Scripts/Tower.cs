9552e6dc9740aa19ab2b9942d9ae8ab4fa28349a
Enrico <keridano@gmail.com>
2020-05-01 17:40:10 +0200

    Added tower looking at enemy

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtEnemy();
+    }
+
+    private void LookAtEnemy()
+    {
+        objectToPan.LookAt(targetEnemy);
+    }
+}
